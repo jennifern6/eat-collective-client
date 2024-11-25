b27b73333b7f700b68c447f4682d84ed9b0594d8
Jennifern6 <jennifer_n@live.ca>
2024-11-25 10:28:24 -0500

    added outlet  in React Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,53 @@ import './App.scss';
 import { 
   createBrowserRouter, 
   RouterProvider, 
+  Outlet, 
 } from 'react-router-dom';
 import Register from './pages/Register';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Single from './pages/Single';
 import Write from './pages/Write';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
-
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Home Page!</div>,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
   },
-  {
-  path: "/register",
-  element: <Register />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    },
 ]);
 
 function App() {
