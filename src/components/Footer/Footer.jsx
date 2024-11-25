31a3ecc87c372d422070081b2bea699beed3d1d9
Jennifern6 <jennifer_n@live.ca>
2024-11-25 14:03:56 -0500

    added navbar/footer components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/images/logo.png";
+import "./Footer.scss";
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>
-      Footer !
-    </div>
-  )
-}
+    <footer className="footer">
+      <div className="footer__container">
+        <img className="footer__logo" src={Logo} alt="Logo" />
+        <span className="footer__text">
+          Copyright Eat Collective ♥️ &copy; 2024 All Rights Reserved
+        </span>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/images/logo.png";
 
 function Navbar() {
   return (
-    <div>
-      Nav bar!
+    <div className="navbar">
+      <div className="navbar__container">
+        <div className="navbar__logo-container">
+          <Link to="/">
+            <img className="navbar__logo" src={Logo} alt="Logo" />
+          </Link>
+        </div>
+
+        <div className="navbar__links">
+          <Link className="link" to="/?cat=link">
+            Link
+          </Link>
+          <Link className="link" to="/?cat=link">
+            Link
+          </Link>
+          <Link className="link" to="/?cat=link">
+            Link
+          </Link>
+          <Link className="link" to="/?cat=link">
+            Link
+          </Link>
+          <Link className="link" to="/?cat=link">
+            Link
+          </Link>
+          <Link className="link" to="/?cat=link">
+            Link
+          </Link>
+          <span>Jen</span>
+          <span>Logout</span>
+          <span>
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
