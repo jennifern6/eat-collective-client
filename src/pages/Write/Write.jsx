d7d9a2c303bcd332f009ca899d4027e1c87fce84
Jennifern6 <jennifer_n@live.ca>
2024-11-25 20:10:17 -0500

    added react-quill dependency for text box in Write component

--- src/pages/Write/Write.jsx
+++ src/pages/Write/Write.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import "./Write.scss"
 
-function Write() {
-  return (
-    <div>
-      Write!
-    </div>
-  )
-}
 
-export default Write
+
+  const Write = () => {
+    const [value, setValue] = useState("");
+
+    return (
+      <div>
+        <div className="write">
+          <div className="write__content">
+            <input type="text" placeholder="Title" />
+            <div className="editorContainer">
+              <ReactQuill theme="snow" value={value} onChange={setValue} />
+            </div>
+          </div>
+          <div className="write__menu">
+            <div className="write__item">item1</div>
+            <div className="write__item">item2</div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  export default Write;
