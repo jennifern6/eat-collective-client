e84b70ffd8ae76dd4d7f102bda237218eb0e02d4
Jennifern6 <jennifer_n@live.ca>
2024-12-01 15:01:12 -0500

    navigate to login works when you register new

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -20,10 +20,14 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:8800/api/auth/register", inputs);
+    
+      await axios.post("http://localhost:8800/api/auth/register", inputs, {
+        withCredentials: true,
+      });
       navigate("/login");
     } catch (err) {
-      setError(err.response.data);
+
+      setError(err.response?.data || "An unexpected error occurred");
     }
   };
 
