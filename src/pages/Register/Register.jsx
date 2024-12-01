8f7221b47eab3b7971b80b7ae2f299ce977492cb
Jennifern6 <jennifer_n@live.ca>
2024-12-01 14:34:46 -0500

    api connection for login and register component works !

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,41 +1,47 @@
-// import React from "react";
-import {Link} from 'react-router-dom';
-import "./Register.scss"
+import { Link, useNavigate } from 'react-router-dom';
+import "./Register.scss";
 import { useState } from 'react';
 import axios from 'axios';
 
-
 const Register = () => {
-
   const [inputs, setInputs] = useState({
-    username:"",
-    email:"",
-    password:"",
-  })
+    username: "",
+    email: "",
+    password: "",
+  });
 
-  const handleChange = e => {
-    setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
-  }
+  const [err, setError] = useState(null);
+  const navigate = useNavigate();
 
-  const handleSubmit = async e => {
-    e.preventDefault ()
-    const res = await axios.post("")
-  }
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8800/api/auth/register", inputs);
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
 
   return (
     <div className="auth">
       <h1>Register</h1>
       <form className="auth__form">
         <input required type="text" placeholder="username" name="username" onChange={handleChange} />
-        <input required type="email" placeholder="email" name="emaile" onChange={handleChange} />
+        <input required type="email" placeholder="email" name="email" onChange={handleChange} />
         <input required type="password" placeholder="password" name="password" onChange={handleChange} />
         <button onClick={handleSubmit}>Register</button>
-        <p>This is an error!</p>
-        <span>Do you have an account? <Link to="/login">Login</Link>
+        {err && <p>{err}</p>}
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
         </span>
       </form>
     </div>
   );
-}
+};
 
 export default Register;
